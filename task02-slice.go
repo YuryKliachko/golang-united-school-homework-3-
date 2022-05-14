2789787603a06c11cdfe6021b9672dcfe007834f
Yury Kliachko <uraklechko@gmail.com>
2022-05-14 12:06:44 +0200

    Task 2

--- task02-slice.go
+++ task02-slice.go
@@ -1,6 +1,9 @@
 package homework
 
 func reverse(input []int64) (result []int64) {
-	//Place your code here
-	return
+	var newSlice []int64
+	for i := len(input) - 1; i > -1; i-- {
+		newSlice = append(newSlice, input[i])
+	}
+	return newSlice
 }
